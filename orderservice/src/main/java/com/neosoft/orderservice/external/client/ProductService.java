01e19640ead0e639fc4cdee47ff62fad912e828c
manish <manish.peddurwar@neosoftmail.com>
2023-10-03 20:19:25 +0530

    commit1

---
+++ orderservice/src/main/java/com/neosoft/orderservice/external/client/ProductService.java
@@ -0,0 +1,20 @@
+package com.neosoft.orderservice.external.client;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="product-service2023/product")
+public interface ProductService {
+
+    @PutMapping("/reduceQuantity/{id}")
+    ResponseEntity<Void> reduceQuantity(
+            @PathVariable("id") long productId,
+            @RequestParam long quantity
+    );
+
+
+}
---
+++ orderservice/src/main/java/com/neosoft/orderservice/request/PaymentMode.java
@@ -0,0 +1,10 @@
+package com.neosoft.orderservice.request;
+
+public enum PaymentMode {
+
+    CASH,
+    PAYPAL,
+    DEBIT_CARD,
+    CREDIT_CARD,
+    APPLE_PAY
+}
